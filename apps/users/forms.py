5b069e34e0007f5379fc526354fb332ac54c39e1
nicom700 <nicom700@gmail.com>
2022-12-15 06:34:00 -0300

    -

--- apps/users/forms.py
+++ apps/users/forms.py
@@ -1,5 +1,4 @@
 from django import forms
-<<<<<<< HEAD
 from django.contrib.auth.forms import UserCreationForm
 
 
@@ -12,16 +11,3 @@ class UserForm(UserCreationForm):
     class Meta:
         model=Users
         fields=["first_name", "last_name", "email", "username", "password1", "password2"]
-=======
-from .models import Users
-
-from django.contrib.auth.forms import UserCreationForm
-
-class UserForm(UserCreationForm):
-    first_name = forms.CharField()
-    last_name = forms.CharField()
-
-    class Meta:
-        model = Users
-        fields = ['first_name', 'last_name', 'email', 'username', 'password1', 'password2']
->>>>>>> login
--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,20 +1,10 @@
 from django.urls import path
-<<<<<<< HEAD
-
 from . import views
+
+from django.contrib.auth import views as auth_views
 
 app_name="users"
 
 urlpatterns = [
     path('register/', views.Registro.as_view(), name="register"),
-=======
-from . import views
-
-from django.contrib.auth import views as auth_views
-
-app_name='users'
-
-urlpatterns = [
-    path('register/', views.Register.as_view(), name='register'),
->>>>>>> login
 ]
