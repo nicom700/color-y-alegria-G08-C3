3e2b4f4ba2f80380c83504821ac9dd6d7f998ec1
nicom700 <nicom700@gmail.com>
2022-12-19 11:36:52 -0300

    se agrega carpeta de fotos al gitignore

--- apps/media/models.py
+++ apps/media/models.py
@@ -3,12 +3,9 @@ from apps.posts.models import Posts
 
 # Create your models here.
 class Media(models.Model):
-    path = models.CharField(max_length=255)
-    filename = models.CharField(max_length=255)
-    original_filename = models.CharField(max_length=255)
-    title = models.CharField(max_length=255)
+    photo = models.ImageField(upload_to='posts', null=True, blank=True)
     is_first = models.BooleanField(default=False)
-    post = models.ForeignKey(Posts, on_delete=models.RESTRICT)
+    post = models.ForeignKey(Posts, on_delete=models.CASCADE)
 
     class Meta:
         managed = True
