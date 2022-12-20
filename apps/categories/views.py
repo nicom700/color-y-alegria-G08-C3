fbb0e2c3f0423dfac02af4000016123c55235db1
Alan Coradi <alanncoradi@gmail.com>
2022-12-20 16:21:03 -0300

    refactorizacion de restricciones a posts y categories

--- apps/categories/views.py
+++ apps/categories/views.py
@@ -4,57 +4,39 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.shortcuts import render, redirect
 from .forms import CategoryForm
 
+from utils.mixins import IsAdminMixin
+
 from apps.categories.models import Categories
 
 # Create your views here.
-class Index(ListView):
+class Index(IsAdminMixin, ListView):
     template_name = 'categories/index.html'
     model = Categories
     context_object_name = 'categories'
     paginate_by = 10
 
-    def dispatch(self, request, *args, **kwargs):
-        if request.user.is_admin:
-            return super().dispatch(request, *args, **kwargs)
-        return redirect('index')
-
     def get_queryset(self):
         productos = Categories.objects.all()
         return productos.order_by('-id')
 
-class Create(CreateView):
+class Create(IsAdminMixin, CreateView):
     model = Categories 
     template_name = 'categories/create.html'
     form_class = CategoryForm
 
-    def dispatch(self, request, *args, **kwargs):
-        if request.user.is_admin:
-            return super().dispatch(request, *args, **kwargs)
-        return redirect('index')
-
     def get_success_url(self):
         return reverse('categories:index')
 
-class Edit(UpdateView):
+class Edit(IsAdminMixin, UpdateView):
     model = Categories
     template_name = 'categories/edit.html'
     form_class = CategoryForm     
 
-    def dispatch(self, request, *args, **kwargs):
-        if request.user.is_admin:
-            return super().dispatch(request, *args, **kwargs)
-        return redirect('index')
-
     def get_success_url(self):
         return reverse('categories:index')
 
-class Delete(DeleteView):
+class Delete(IsAdminMixin, DeleteView):
     model = Categories
-
-    def dispatch(self, request, *args, **kwargs):
-        if request.user.is_admin:
-            return super().dispatch(request, *args, **kwargs)
-        return redirect('index')
 
     def get_success_url(self):
         return reverse('categories:index')
--- apps/posts/views.py
+++ apps/posts/views.py
@@ -9,20 +9,17 @@ from .forms import PostForm
 
 from slugify import slugify 
 
+from utils.mixins import IsAdminMixin, is_admin_required
+
 from apps.posts.models import Posts
 from apps.categories.models import Categories
 
 # Create your views here.
-class Index(ListView):
+class Index(IsAdminMixin, ListView):
     template_name = 'posts/index1.html'
     model = Posts
     context_object_name = 'posts'
     paginate_by = 10
-
-    def dispatch(self, request, *args, **kwargs):
-        if request.user.is_admin:
-            return super().dispatch(request, *args, **kwargs)
-        return redirect('index')
     
     def get_context_data(self, *args, **kwargs):
         posts = Posts.objects.all()
@@ -35,81 +32,75 @@ class Index(ListView):
         #return productos.order_by('-id')
       #  return categories.order_by('-id')
 
+@is_admin_required()
 def Create(request):
-    if request.user.is_admin:
-        form = PostForm()
-        categories = Categories.objects.all()
+    form = PostForm()
+    categories = Categories.objects.all()
 
-        if request.method == 'POST':
-            form = PostForm(request.POST, request.FILES)
-            if form.is_valid():
-                post = form.save(commit=False)
-                post.user_id = request.user.id
-                post.slug = slugify(post.title)
-                post.save()
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user_id = request.user.id
+            post.slug = slugify(post.title)
+            post.save()
 
-                post_id = Posts.objects.get(id=post.id)
+            post_id = Posts.objects.get(id=post.id)
 
-                if 'first_photo' in request.FILES:
-                    first_photo = request.FILES.get('first_photo')
-                    Media.objects.create(photo = first_photo, is_first=True, post = post_id)
+            if 'first_photo' in request.FILES:
+                first_photo = request.FILES.get('first_photo')
+                Media.objects.create(photo = first_photo, is_first=True, post = post_id)
 
-                if 'photos' in request.FILES:
-                    photos = request.FILES.getlist('photos')
-                    for photo in photos:
-                        Media.objects.create(photo = photo, post = post_id)
+            if 'photos' in request.FILES:
+                photos = request.FILES.getlist('photos')
+                for photo in photos:
+                    Media.objects.create(photo = photo, post = post_id)
 
-                return redirect('posts:index')
+            return redirect('posts:index')
 
-        form_ = {
-            'categories': categories,
-            'form': form,
-        }
-        return render(request, 'posts/create.html', form_)
-    return redirect('index')
+    form_ = {
+        'categories': categories,
+        'form': form,
+    }
+    return render(request, 'posts/create.html', form_)
 
+@is_admin_required()
 def Edit(request, pk):
-    if request.user.is_admin:
-        post = Posts.objects.get(pk=pk)
-        form = PostForm(instance=post)
-        categories = Categories.objects.all()
+    post = Posts.objects.get(pk=pk)
+    form = PostForm(instance=post)
+    categories = Categories.objects.all()
 
-        if request.method == 'POST':
-            form = PostForm(request.POST, request.FILES)
-            if form.is_valid():
-                post.title = request.POST['title']
-                post.slug = slugify(request.POST['title'])
-                post.body = request.POST['body']
-                post.category = Categories.objects.get(id=request.POST['category'])
-                post.save()
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post.title = request.POST['title']
+            post.slug = slugify(request.POST['title'])
+            post.body = request.POST['body']
+            post.category = Categories.objects.get(id=request.POST['category'])
+            post.save()
 
-                post_id = Posts.objects.get(id=post.id)
+            post_id = Posts.objects.get(id=post.id)
 
-                if 'first_photo' in request.FILES:
-                    first_photo = request.FILES.get('first_photo')
-                    Media.objects.create(photo = first_photo, is_first=True, post = post_id)
+            if 'first_photo' in request.FILES:
+                first_photo = request.FILES.get('first_photo')
+                Media.objects.create(photo = first_photo, is_first=True, post = post_id)
 
-                if 'photos' in request.FILES:
-                    photos = request.FILES.getlist('photos')
-                    for photo in photos:
-                        Media.objects.create(photo = photo, post = post_id)
+            if 'photos' in request.FILES:
+                photos = request.FILES.getlist('photos')
+                for photo in photos:
+                    Media.objects.create(photo = photo, post = post_id)
 
-                return redirect('posts:index')
-        
-        form_ = {
-            'categories': categories,
-            'form': form,
-        }
-        return render(request, 'posts/edit.html', form_)
-    return redirect('index')
+            return redirect('posts:index')
+    
+    form_ = {
+        'categories': categories,
+        'form': form,
+    }
+    return render(request, 'posts/edit.html', form_)
+    
 
-class Delete(DeleteView):
+class Delete(IsAdminMixin, DeleteView):
     model = Posts
-
-    def dispatch(self, request, *args, **kwargs):
-        if request.user.is_admin:
-            return super().dispatch(request, *args, **kwargs)
-        return redirect('index')
 
     def get_success_url(self):
         return reverse('posts:index')
