84e22f6959c1ce88990677ab5a288258ab70a073
Kokoteros <marcosjose.piedra@gmail.com>
2022-12-20 21:23:27 -0300

    sección Nosotros

--- color_y_alegria/views.py
+++ color_y_alegria/views.py
@@ -4,3 +4,8 @@ def index(request):
     template_name = 'index.html'
     
     return render(request, template_name)
+
+def nosotros(request):
+    template_name = 'nosotros/nosotros.html'
+
+    return render(request, template_name)
